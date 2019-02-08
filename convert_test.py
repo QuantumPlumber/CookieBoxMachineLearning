d42080b931d827d67c726b22c49f5460fb579510
QuantumPlumber <44450703+QuantumPlumber@users.noreply.github.com>
2019-02-07 18:35:04 -0700

    Working on data conversion

--- convert_test.py
+++ convert_test.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 
-from DataHandling import convert_2_TFRecord
+from DataHandling import transform_2_TFRecord
 
-convert_2_TFRecord()
+transform_2_TFRecord()
 
