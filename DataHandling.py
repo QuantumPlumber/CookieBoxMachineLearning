fa566b45f8e27c39f667c52f8e9adaeb778bf127
QuantumPlumber <44450703+QuantumPlumber@users.noreply.github.com>
2019-03-20 13:34:26 -0700

    New Functions for processing new multiprocessor output format

--- DataHandling.py
+++ DataHandling.py
@@ -400,3 +400,115 @@ def transform_2_TFRecord(filename='../AttoStreakSimulations/TF_train_single_puls
     h5file.close()
 
     return
+
+
+def gaussian_kernel_compute_mp(energy_points, Spectra):
+    # Convenience function to ensure destruction of intermediate large arrays.
+
+    # reshape Spectra array before processing:
+    local_spectra_shape = Spectra.shape
+    local_spectra_reshape = np.array(
+        [local_spectra_shape[0] * local_spectra_shape[1], local_spectra_shape[2], local_spectra_shape[3]])
+    spectra_reshaped = np.reshape(Spectra, newshape=local_spectra_reshape)
+
+    #  use the old trick of setting all zero values to some outrageous number.
+    #  No compute penalty as we already compute full array.
+    #  Solves problem of zero padding at end of ragged arrays.
+    energy_points_array = np.multiply.outer(np.ones_like(Spectra), energy_points)
+    Spectra[Spectra == 0.0] = 1000
+    gaussian_centers = np.multiply.outer(Spectra, np.ones_like(energy_points))
+
+    # Compute gaussian Kernel Density estimate over energy_points
+    # waveforms must be summed over cookie
+    waveforms = np.sum(np.exp(-(energy_points_array - gaussian_centers) ** 2 / (2 * .25 / 2.355)),
+                       axis=2)
+
+    # print(waveforms)
+    return waveforms
+
+
+def transform_2_spectra_from_mp(filename='../AttoStreakSimulations/TF_train_single.hdf5',
+                                transfer='reformed_spectra.hdf5'):
+    '''
+    Transforms the raw simulation data into detector data of a given precision
+
+    Combines the first and second dimension for easier shuffling.
+    :param filename:
+    :return:
+    '''
+
+    h5file = h5py.File(filename, 'r')
+    h5_reformed = h5py.File(transfer)
+
+    if 'Hits' in h5file:
+        Hits = h5file['Hits']
+    else:
+        raise Exception('No "Hits" in file.')
+
+    if 'Spectra' in h5file:
+        Spectra = h5file['Spectra']
+    else:
+        raise Exception('No "Spectra" in file.')
+
+    if 'VN_coeff' in h5file:
+        VN_coeff = h5file['VN_coeff']
+    else:
+        raise Exception('No "VN_coeff" in file.')
+
+    num_ebins = 100
+    energy_range = 100.  # 100 eV was used in the code
+    energy_points = np.linspace(0, energy_range, num_ebins)
+
+    spectra_shape = np.array(Spectra.shape)
+    # print(shape)
+    spectra_reshape = np.array([spectra_shape[0] * spectra_shape[1], 16, num_ebins])
+    if 'Spectra16' not in h5_reformed:
+        detectors_ref = h5_reformed.create_dataset(name='Spectra16', compression='gzip', shape=spectra_reshape.tolist())
+    else:
+        detectors_ref = h5_reformed['Spectra16']
+
+    vn_shape = np.array(VN_coeff.shape)
+    vn_reshape = np.append(spectra_reshape[0], vn_shape[1] * vn_shape[2])
+    if 'VN_coeff' not in h5_reformed:
+        vn_co_ref = h5_reformed.create_dataset(name='VN_coeff', shape=vn_reshape.tolist(), compression='gzip',
+                                               dtype='complex128')
+    else:
+        vn_co_ref = h5_reformed['VN_coeff']
+
+    hits_shape = np.array(Hits.shape)
+    hits_reshape = np.array(hits_shape[0] * hits_shape[1], hits_shape[2])
+    if 'Hits' not in h5_reformed:
+        hits_ref = h5_reformed.create_dataset(name='Hits', shape=(hits_reshape.tolist(),))
+    else:
+        hits_ref = h5_reformed['Hits']
+    # hits_ref = np.reshape(Hits, hits_reshape)
+
+    num_spectra = np.arange(0, spectra_shape[0], step=100)
+    num_spectra_top = np.append(num_spectra, np.array(spectra_shape[0]) - 1)[1:]
+    bot_top = np.column_stack((num_spectra, num_spectra_top)).astype('int')
+
+    def data_generator(Hits, Spectra, VN_coeff, jump):
+        for i in np.arange(0, spectra_shape[0], step=jump):
+            yield Hits[i:i + jump, ...], Spectra[i:i + jump, :, :, 0, :], VN_coeff[i:i + jump, ...], [i, i + jump]
+
+    sim_data = data_generator(Hits=Hits, Spectra=Spectra, VN_coeff=VN_coeff, jump=100)
+
+    # Encapsulate memory heavy things in the slicer function so python is forced to free memory
+    for hh, ss, vnvn, b_slice in data_generator:
+        checkpoint = time.process_time()
+
+        # reshape and record hits
+        local_hits_shape = hh.shape
+        local_hits_reshape = np.array(local_hits_shape[0] * local_hits_shape[1], local_hits_shape[2])
+        hits_ref[b_slice[0] * hits_shape[1]:b_slice[1] * hits_shape[1], :] = np.reshape(hh, newshape=local_hits_reshape)
+
+        # Create 16 detector lists, discards angle information.
+        transformed_spectra = gaussian_kernel_compute_mp(energy_points=energy_points, Spectra=ss)
+
+        print('complete {} to {} of {}'.format(b_slice[0], b_slice[1], spectra_shape[0]))
+
+        delta_t = checkpoint - time.process_time()
+        print('Converted in {}'.format(delta_t))
+
+    h5file.close()
+    h5_reformed.close()
--- convert_test.py
+++ convert_test.py
@@ -7,7 +7,7 @@ importlib.reload(DH)
 DH.transform_2_spectra(filename='../AttoStreakSimulations/TF_training_densespace.hdf5',
                        transfer='reformed_spectra_densesapce.hdf5')
 
-h5_file = h5py.File('reformed_spectra_densesapce.hdf5','r')
+h5_file = h5py.File('reformed_spectra_densesapce.hdf5', 'r')
 print(h5_file.keys())
 for key in list(h5_file.keys()):
     print('shape of {} is {}'.format(key, h5_file[key].shape))
