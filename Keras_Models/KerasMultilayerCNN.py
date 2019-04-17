d686819f99d9f7f0561767d82047178b73e024eb
QuantumPlumber <44450703+QuantumPlumber@users.noreply.github.com>
2019-04-17 16:39:46 -0700

    Keras Models

--- Keras_Models/KerasMultilayerCNN.py
+++ Keras_Models/KerasMultilayerCNN.py
@@ -12,8 +12,13 @@ def slicer(inputs, i):
 
 
 slicing = tf.keras.layers.Lambda(slicer)
-conv1 = tf.keras.layers.Conv1D(filters=12, kernel_size=10, padding='valid')  # no zero padding
+conv1 = tf.keras.layers.Conv1D(filters=10, kernel_size=10, padding='valid')  # no zero padding
 pool1 = tf.keras.layers.MaxPooling1D(pool_size=10, strides=1)
+conv2 = tf.keras.layers.Conv1D(filters=20, kernel_size=10, padding='valid')  # no zero padding
+pool2 = tf.keras.layers.MaxPooling1D(pool_size=20, strides=1)
+conv3 = tf.keras.layers.Conv1D(filters=60, kernel_size=10, padding='valid')  # no zero padding
+#pool3 = tf.keras.layers.MaxPooling1D(pool_size=45, strides=1)
+pool3 = tf.keras.layers.MaxPooling1D(pool_size=30, strides=1)
 spectra16_conv_list = []
 
 for spect in range(16):
@@ -21,12 +26,16 @@ for spect in range(16):
     sp = slicing(spectra16_reshaped)
     con = conv1(sp)
     pool = pool1(con)
+    con = conv2(pool)
+    pool = pool2(con)
+    con = conv3(pool)
+    pool = pool3(con)
     spectra16_conv_list.append(pool)
 
 net = tf.keras.layers.Concatenate()(spectra16_conv_list)
 net = tf.keras.layers.Flatten()(net)
 
-dense_network = [2304, 1152, 1152, 500, 500]
+dense_network = [960, 960, 500]
 for nodes in dense_network[:-1]:
     net = tf.keras.layers.Dense(units=nodes)(net)
 
@@ -41,6 +50,14 @@ keras_model = tf.keras.Model(inputs=spectra16, outputs=[mag, phase_dot])
 adadelta = tf.keras.optimizers.Adadelta(lr=.01, rho=0.95, epsilon=1e-8, decay=0.0)
 keras_model.compile(optimizer=adadelta,
                     loss={'magnitude': 'mean_squared_error', 'magphase': 'mean_squared_error'},
-                    loss_weights=[phase_scale_factor, 1.])
+                    loss_weights=[phase_scale_factor**2, 1.])
 
-plot_model(keras_model, to_file='model.png', show_shapes=True)
+#direct = './multilayer_cnn'
+direct = './multilayer_cnn_big'
+filename = direct+'/'+'model.png'
+plot_model(keras_model, to_file=filename, show_shapes=True)
+
+filename = direct+'/'+'model.json'
+f = open(filename,'w')
+f.write(keras_model.to_json())
+f.close()
--- Keras_Models/keras_fit.py
+++ Keras_Models/keras_fit.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import h5py
+import os
+import matplotlib.pyplot as plt
 
 
-def data_generator(transfer='TF_train_wave_unwrapped_eggs.hdf5', batch_size=64, cut_bot=.8, cut_top=1.):
+mag_scale_factor = 100
+phase_scale_factor = 1200 * np.pi
+
+def data_generator(transfer='TF_train_wave_unwrapped_eggs.hdf5', batch_size=64, cut_bot=.8, cut_top=1., reps=20):
     '''
         Reformats transformed simulation data into TFRecord data format
 
@@ -25,43 +30,77 @@ def data_generator(transfer='TF_train_wave_unwrapped_eggs.hdf5', batch_size=64, 
 
     random_shuffled_index = np.arange(0, Spectra16.shape[0])[
                             int(Spectra16.shape[0] * cut_bot):int(Spectra16.shape[0] * cut_top)]
-    np.random.shuffle(random_shuffled_index)
+    random_shuffled_index = np.tile(random_shuffled_index, reps=reps)
+    # np.random.shuffle(random_shuffled_index)
 
-    print((int(Spectra16.shape[0] * cut_bot),int(Spectra16.shape[0] * cut_top)))
+    print((int(random_shuffled_index.shape[0] * cut_bot), int(random_shuffled_index.shape[0] * cut_top)))
 
     for batch in np.arange(start=0, stop=random_shuffled_index.shape[0], step=batch_size):
+        magnitude = phase_scale_factor*Pulse_truth[np.sort(random_shuffled_index[batch: batch + batch_size]), 0, :]
+        phase = Pulse_truth[np.sort(random_shuffled_index[batch: batch + batch_size]), 1, :]
+        magphase = magnitude * phase/phase_scale_factor
         yield (Spectra16[np.sort(random_shuffled_index[batch: batch + batch_size]), ...],
-               {'magnitude': Pulse_truth[np.sort(random_shuffled_index[batch: batch + batch_size]), 0, :],
-                'magphase': Pulse_truth[np.sort(random_shuffled_index[batch: batch + batch_size]), 1, :]})
+               {'magnitude': magnitude,
+                'phase': magphase})
 
 
+train_data = data_generator(transfer='../Data/unwrapped/Eggs/TF_train_wave_unwrapped_eggs.hdf5',
+                            batch_size=64,
+                            cut_bot=.0,
+                            cut_top=.8)
+test_data = data_generator(transfer='../Data/unwrapped/Eggs/TF_train_wave_unwrapped_eggs.hdf5',
+                           batch_size=64,
+                           cut_bot=.8,
+                           cut_top=1.0)
 
-train_data = data_generator(transfer='../TF_train_wave_unwrapped_eggs.hdf5', batch_size=64, cut_bot=.0, cut_top=.8)
-test_data = data_generator(transfer='../TF_train_wave_unwrapped_eggs.hdf5', batch_size=64, cut_bot=.8, cut_top=1.0)
-
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='./tensorboard',
+# direct = './multilayer_cnn'
+#direct = './multilayer_cnn_big'
+direct = './multilayer_cnn_5'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=direct,
                                                       histogram_freq=1,
                                                       batch_size=64,
                                                       write_graph=True,
                                                       write_grads=False,
                                                       write_images=False)
+'''
+keras.callbacks.ModelCheckpoint(filepath,
+                                monitor='val_loss',
+                                verbose=0,
+                                save_best_only=False,
+                                save_weights_only=False,
+                                mode='auto',
+                                period=1)
+'''
 
-keras_model.fit_generator(train_data,
-                          steps_per_epoch=int(1e3),
-                          epochs=1,
-                          verbose=1,
-                          callbacks=[tensorboard_callback],
-                          validation_data=test_data,
-                          validation_steps=int(1e1),
-                          class_weight=None,
-                          max_queue_size=10,
-                          workers=1,
-                          use_multiprocessing=False,
-                          shuffle=True)
-'''
-keras_model.fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.0,
-                validation_data=None,
-                shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None,
-                validation_steps=None,
-                validation_freq=1)
-'''
+filename = direct+'/'+'saved_model.h5'
+try:
+    keras_model
+except NameError:
+    print('no keras model in memory')
+    if os.path.isfile(filename):
+        keras_model = tf.keras.models.load_model(filepath=filename)
+else:
+    print('keras_model already instantiated')
+
+
+history = keras_model.fit_generator(train_data,
+                                    steps_per_epoch=int(1e3),
+                                    epochs=10,
+                                    verbose=1,
+                                    callbacks=[tensorboard_callback],
+                                    validation_data=test_data,
+                                    validation_steps=int(1e1),
+                                    class_weight=None,
+                                    max_queue_size=10,
+                                    workers=1,
+                                    use_multiprocessing=False)
+
+tf.keras.models.save_model(model=keras_model, filepath=filename)
+#del keras_model
+
+fig, axs = plt.subplots(6, 1, constrained_layout=True, figsize=(6, 18))
+for i, key in enumerate(history.history):
+    axs[i].plot(history.history[key])
+    axs[i].set_title(key)
+
+fig.savefig(direct + '/' + '/history.png', dpi=700)
